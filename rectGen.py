9dc2bbe39b7c555103fbea444a2dd7d971d7aa16
bigcheez9315 <as5244@columbia.edu>
2018-01-29 16:08:48 -0500

    created squareGen.py
    This program takes length and width as input from the reader and automatically generates a .svg file with the proper dimensions. I tested it and it works

---
+++ rectGen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 15:25:36 2018
+
+@author: Steinneman
+"""
+sL = 36 # Sheet length in inches
+sW = 24 # Sheet width in inches
+
+# I think the units are in pixels so let's convert from inches to pixels. 
+# One inch is equivalent to 75 pixels according to what I found online
+def in2Px(inch):
+    return inch*225
+
+U = ""
+while U.lower() not in ("in", "mm"):
+    U = input("Box Units (mm or in): ")
+print("Selected: " + str(U.lower()))
+
+
+while True:
+    try:
+        L = int(input("Square Length: "))
+        W = int(input("Square Width: "))
+        break
+    except ValueError:
+        print("Oops! That was not a valid number! Try again...")
+        
+if U.lower() == "in":
+    L = L*75
+    W = W*75       
+
+
+filename = "rectGen.svg"
+f = open(filename, "w+")
+f.write("""<?xml version="1.0" encoding="UTF-8" ?>\n""")
+f.write("""<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n""")
+
+f.write("""<rect x="0" y="0" width="%d" height="%d" style="stroke-width:1;
+        stroke:rgb(0,0,0);fill:none" />\n""" %(W, L))
+f.write("</svg>")
+f.close()
