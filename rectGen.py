ba1d9065d43d0e48b847096e0d99f0f69335a088
Nikita Komachkov <35057647+nikitakomachkov@users.noreply.github.com>
2018-02-02 00:45:46 -0500

    Update rectGen.py
    Correctly updated the units by adding 'mm' after value. Updated stroke width to 0.001", an industry standard for vector cutting.

--- rectGen.py
+++ rectGen.py
@@ -2,24 +2,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 29 15:25:36 2018
-
-@author: Steinneman
+Modified on Thur Feb 2 12:39 2018
+@author: Steinneman & Komachkov
 """
-sL = 36 # Sheet length in inches
-sW = 24 # Sheet width in inches
-
-# I think the units are in pixels so let's convert from inches to pixels. 
-# One inch is equivalent to 75 pixels according to what I found online
-def in2Px(inch):
-    return inch*75
-def mm2Px(mm):
-    return (mm/25.4)*75
+def in2mm(inch):
+    return inch*25.4
 
 U = ""
 while U.lower() not in ("in", "mm"):
     U = input("Box Units (mm or in): ")
 print("Selected: " + str(U.lower()))
-
 
 while True:
     try:
@@ -30,20 +22,17 @@ while True:
         print("Oops! That was not a valid number! Try again...")
         
 if U.lower() == "in":
-    L = in2Px(L)
-    W = in2Px(W)
-    
-if U.lower() == "mm":
-    L = mm2Px(L)
-    W = mm2Px(W)
-
+    L = in2mm(L)
+    W = in2mm(W)
 
 filename = "rectGen.svg"
 f = open(filename, "w+")
 f.write("""<?xml version="1.0" encoding="UTF-8" ?>\n""")
 f.write("""<svg xmlns="http://www.w3.org/2000/svg" version="1.1">\n""")
 
-f.write("""<rect x="0" y="0" width="%d" height="%d" style="stroke-width:1;
-        stroke:rgb(0,0,0);fill:none" />\n""" %(W, L))
+# Note that width and height are in mm
+# The rule of thumb is to set the vector line to 0.001 in.
+f.write("""<rect x="0" y="0" width="%dmm" height="%dmm" style="stroke-width:0.0254mm;
+        stroke:rgb(255,0,0);fill:none" />\n""" %(W, L))
 f.write("</svg>")
 f.close()
